2fbaf7c2690247c665329a847e9a96c0168b36ec
Matt Clarke <matt.clarke@ess.eu>
2023-07-31 10:59:22 +0200

    rust returns the buffer as a binary

--- native/flatbuffers/src/lib.rs
+++ native/flatbuffers/src/lib.rs
@@ -6,9 +6,11 @@ extern crate flatbuffers;
 mod f144_logdata_generated;
 pub use f144_logdata_generated::{root_as_f_144_log_data, f144_LogData, Value};
 use crate::f144_logdata_generated::{f144_LogDataArgs, Double, DoubleArgs};
+use rustler::types::binary::{NewBinary, Binary};
+use rustler::{Env};
 
 #[rustler::nif]
-fn convert_flatbuffer_double(source_name : &str, timestamp: i64, value: f64) -> Vec<u8>{
+fn convert_flatbuffer_double<'a>(env: Env<'a>, source_name : &str, timestamp: i64, value: f64) -> Binary<'a>{
     let mut builder = flatbuffers::FlatBufferBuilder::with_capacity(1024);
     let source = builder.create_string(source_name);
     let log_value= Double::create(&mut builder, &DoubleArgs {
@@ -25,7 +27,10 @@ fn convert_flatbuffer_double(source_name : &str, timestamp: i64, value: f64) -> 
 
     builder.finish(logdata, Option::from("f144"));
     let buf = builder.finished_data();
-    return buf.to_vec();
+
+    let mut new_binary = NewBinary::new(env, buf.len());
+    new_binary.copy_from_slice(buf);
+    return Binary::from(new_binary);
 }
 
 rustler::init!("Elixir.FlatBuffers", [convert_flatbuffer_double]);
