8939c1d2a0824df46952304de6c2528682ec62b3
Matt Clarke <matt.clarke@ess.eu>
2023-08-03 08:06:16 +0200

    will send updates periodically

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-# Elixir and EPICS
+# Elixir EPICS forwarder
 
-An example of using a GenServer with a Port to call an external program, in this case pvmonitor.
+A recreation of the EPICS Forwarder but in Elixir.
+
+The motivation is to get more experience using Elixir and OTP by recreating an application I am familiar with.
+
+Interesting things it does:
+ - Uses GenServer(s)
+ - Uses a Port to call an external program, in this case pvmonitor
+ - Has a dynamic supervisor
+ - Talks to Apache Kafka
+ - Uses Rustler to call Rust code
 
 ## Running
 First time only:
@@ -20,12 +29,17 @@ From within iex:
 ```
 
 ## How it works
+### Ports
 - The program creates a GenServer that uses the [Port module](https://hexdocs.pm/elixir/Port.html) to create a pvmonitor process to monitor a port.
 - The call is via the `run_wrapper` script as that makes sure that the pvmonitor process is cleaned up if the GenServer is stopped. See [zombie operating system processes](https://hexdocs.pm/elixir/Port.html#module-zombie-operating-system-processes) for more information.
 - The port passes data to the GenServer via stdout. 
 - pvmonitor is configured to format the PV data as JSON, so it is simple to recreate it in the GenServer.
 
+### Apache Kafka
+- Mostly taken care of by Brod.
+
 ## Possible improvements
-- The PV is currently hard-coded.
-- Be able to monitor multiple PVs using multiple BEAM processes.
+- Store the flatbuffer in the state, so we don't need to create it on when resending it periodically.
+- The topic is hard-coded.
+- Use ETS just for experience of using it.
 - Could the need for the `run_wrapper` be removed by registering the pid of the port and killing it in the supervisor when required. I guess it would be orphaned if BEAM crashed though, so perhaps not a great idea...
--- lib/elixir_epics/monitor.ex
+++ lib/elixir_epics/monitor.ex
@@ -22,12 +22,29 @@ defmodule ElixirEpics.Monitor do
 
     Logger.info("pid = #{Port.info(port)[:os_pid]}")
 
-    {:ok, %{port: port, connected: False, latest_data: %{}, exit_status: nil}}
+    {:ok,
+     %{
+       port: port,
+       connected: False,
+       latest_data: %{},
+       exit_status: nil,
+       has_connected: false,
+       pvname: pvname
+     }}
   end
 
   def terminate(reason, %{port: _port} = _state) do
     Logger.warning("Terminated: #{reason}")
     :normal
+  end
+
+  # Triggered by a timer
+  def handle_info(:update, state) do
+    generate_flatbuffer_for_double(state[:pvname], state[:latest_data])
+    |> send_to_kafka("test_topic")
+
+    schedule_update()
+    {:noreply, state}
   end
 
   # Triggered when the port uses STDOUT
@@ -41,7 +58,8 @@ defmodule ElixirEpics.Monitor do
       :ok ->
         updated = Map.merge(state.latest_data, data)
         Logger.info("Data: #{inspect(updated)}")
-        generate_flatbuffer_for_double(pv, updated) |> send_to_kafka("test_topic")
+        generate_flatbuffer_for_double(state[:pvname], updated) |> send_to_kafka("test_topic")
+        state = on_first_connection(state)
         {:noreply, %{state | latest_data: updated, connected: true}}
 
       :error ->
@@ -76,15 +94,33 @@ defmodule ElixirEpics.Monitor do
     {:noreply, state}
   end
 
-  defp generate_flatbuffer_for_double(pv, data) do
+  defp generate_flatbuffer_for_double(pvname, data) do
     %{"timeStamp" => %{"secondsPastEpoch" => seconds, "nanoseconds" => nanoseconds}} = data
     timestamp_ns = seconds * 1_000_000_000 + nanoseconds
-    buffer = FlatBuffers.convert_flatbuffer_double(pv, timestamp_ns, data["value"])
+    buffer = FlatBuffers.convert_flatbuffer_double(pvname, timestamp_ns, data["value"])
     timestamp_ms = trunc(timestamp_ns / 1_000)
     {timestamp_ms, buffer}
   end
 
   defp send_to_kafka({timestamp_ms, buffer}, topic) do
     :brod.produce_sync(:kafka_client, topic, :hash, <<>>, {timestamp_ms, buffer})
+  end
+
+  defp on_first_connection(state) do
+    %{has_connected: is_first_time} = state
+
+    case is_first_time do
+      false ->
+        state
+        schedule_update()
+        %{state | has_connected: true}
+
+      true ->
+        state
+    end
+  end
+
+  defp schedule_update() do
+    Process.send_after(self(), :update, 5000)
   end
 end
